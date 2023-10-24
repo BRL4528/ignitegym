e059a9efe794699f70169e5ca3a31db682ee2d20
brl4528 <blgc.sgo@hotmail.com>
2023-10-24 05:08:48 -0400

    add type and context auth user

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 import { THEME } from './src/theme';
 
 import { Routes } from './src/routes';
+import { AuthContext } from '@contexts/AuthContext';
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -16,7 +17,16 @@ export default function App() {
        backgroundColor="transparent"
        translucent 
       />
+      <AuthContext.Provider value={{
+        user: {
+          id: '1',
+          name: '2',
+          email: '2@example.com',
+          avatar: 'http://example.com'
+        }
+      }}>
       { fontsLoaded ? <Routes /> : <Loading />}
+      </AuthContext.Provider>
     </NativeBaseProvider>
   );
 }
