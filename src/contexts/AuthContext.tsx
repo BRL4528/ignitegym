56c30b2c84349d1cf67c03ca4a9e0519a36a8eb3
brl4528 <blgc.sgo@hotmail.com>
2023-10-30 05:00:20 -0400

    create logic Asyncstorage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,36 +1,51 @@
-import { createContext, ReactNode, useState } from "react";
-import { UserDTO } from "@dtos/UserDTO";
-import { api } from "@services/api";
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import { UserDTO } from '@dtos/UserDTO';
+import { api } from '@services/api';
+import { storageUserSave, storageUserGet } from '@storage/storageUser';
 
 export type AuthContextDataProps = {
   user: UserDTO;
   signIn: (email: string, password: string) => Promise<void>;
-}
+};
 
 type AuthContextProviderProps = {
   children: ReactNode;
-}
+};
 
+export const AuthContext = createContext<AuthContextDataProps>(
+  {} as AuthContextDataProps
+);
 
-export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
+export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [user, setUser] = useState<UserDTO>({} as UserDTO);
 
-export function AuthContextProvider({children}: AuthContextProviderProps) {
- const [user, setUser] = useState<UserDTO>({} as UserDTO );
-
-async function signIn(email: string, password: string) {
-  try {
-    const { data } = await api.post('/sessions', { email, password })
-    if(data.user) {
-      setUser(data.user)
+  async function signIn(email: string, password: string) {
+    try {
+      const { data } = await api.post('/sessions', { email, password });
+      if (data.user) {
+        setUser(data.user);
+        storageUserSave(data.user);
+      }
+    } catch (error) {
+      throw error;
     }
-  } catch (error) {
-    throw error
   }
-}
+
+  async function loadUserData() {
+    const userLogged = await storageUserGet();
+
+    if (userLogged) {
+      setUser(userLogged);
+    }
+  }
+
+  useEffect(() => {
+    loadUserData()
+  }, [])
 
   return (
-    <AuthContext.Provider value={{user, signIn}}>
-  {children}
-  </AuthContext.Provider>
-    )
+    <AuthContext.Provider value={{ user, signIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
